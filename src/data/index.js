9fdb2165cdc3bac1d31ec31b75c2ea0046615708
godwin <your_mail>
2026-03-19 09:25:01 +0100

    Created src/data/index to add data for my sidebar

---
+++ src/data/index.js
@@ -0,0 +1,10 @@
+export const myData = {
+	name : "Ohazulike Chukwuebuka Godwin",
+	tagline : "A Software Engineer who builds scalable products and loves to solve problems.",
+	photo : "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773908144/IMG-20230528-WA0045_2_pjqjzx.jpg",
+	  social: {
+    github:   "https://github.com/godwinn22",
+    twitter:  "https://x.com/Pandas_Stack",
+    linkedin: "https://www.linkedin.com/in/ohazulike-godwin"
+  }
+}
