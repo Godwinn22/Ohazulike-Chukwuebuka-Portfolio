073d06c964efd0bdcb872f9c6d5ae8ea270ad811
godwin <your_mail>
2026-03-19 12:31:32 +0100

    Created the Home component.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import { statsData, recentProjects } from "../data"
+import { FaArrowRight } from "react-icons/fa"
+
+const Home = () => {
+  return (
+	<div>Home</div>
+  )
+}
+
+export default Home
