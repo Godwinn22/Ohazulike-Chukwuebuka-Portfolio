a69021b7083a569cabe818fb60eb8684446676f4
godwin <your_mail>
2026-03-19 10:45:20 +0100

    update profile photo URL in myData

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,7 @@
 export const myData = {
 	name : "Ohazulike Chukwuebuka Godwin",
 	tagline : "A Software Engineer who builds scalable products and loves to solve problems.",
-	photo : "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773908144/IMG-20230528-WA0045_2_pjqjzx.jpg",
+	photo : "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773911343/IMG-20230528-WA0041_4_nhhnoz.jpg",
 	  social: {
     github:   "https://github.com/godwinn22",
     twitter:  "https://x.com/Pandas_Stack",
