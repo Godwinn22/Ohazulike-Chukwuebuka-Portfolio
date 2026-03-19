3ee6680336a1b1e64fdc8ccc13fa5388a3f91bca
godwin <your_mail>
2026-03-19 12:04:16 +0100

    Added project data for my homepage

--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,38 @@
 export const myData = {
-	name : "Ohazulike Chukwuebuka Godwin",
-	tagline : "A Software Engineer who builds scalable products and loves to solve problems.",
-	photo : "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773911343/IMG-20230528-WA0041_4_nhhnoz.jpg",
-	  social: {
-    github:   "https://github.com/godwinn22",
-    twitter:  "https://x.com/Pandas_Stack",
-    linkedin: "https://www.linkedin.com/in/ohazulike-godwin"
-  }
-}
+    name: "Ohazulike Chukwuebuka Godwin",
+    tagline:
+        "A Software Engineer who builds scalable products and loves to solve problems.",
+    photo: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773911343/IMG-20230528-WA0041_4_nhhnoz.jpg",
+    social: {
+        github: "https://github.com/godwinn22",
+        twitter: "https://x.com/Pandas_Stack",
+        linkedin: "https://www.linkedin.com/in/ohazulike-godwin",
+    },
+};
+
+export const statsData = [
+    { number: "3", label: "YEARS OF\nEXPERIENCE" },
+    { number: "+8", label: "PROJECTS\nCOMPLETED" },
+    { number: "+5", label: "HAPPY\nCLIENTS" },
+];
+
+export const recentProjects = [
+    {
+        id: 1,
+        title: "Asset Pulse HQ",
+        category: "Asset Tracker Dashboard",
+        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773918152/Macbook-Air-assetpulsehq-dashboard_zpreik.png",
+    },
+    {
+        id: 2,
+        title: "Setime Digital Hub Academy",
+        category: "Coding Academy Platform",
+        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837457/Setime_Digital_Hub_bq54ln.png",
+    },
+    {
+        id: 3,
+        title: "Vermac Educational Foundation",
+        category: "NGO Foundation",
+        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837439/vermac_index_ccyhss.png",
+    },
+];
