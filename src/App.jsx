5e18d966139e7847327108960c72dcbf65480f57
godwin <your_mail>
2026-03-19 11:33:18 +0100

    Refactored App component to include Navbar and wrap with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Sidebar />
-		</>
+            <Navbar />
+        </BrowserRouter>
     );
 };
 
