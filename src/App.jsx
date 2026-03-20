c8daede0ca49598d579b9afadb9fe899a4e5780d
godwin <your_mail>
2026-03-20 12:42:36 +0100

    Added routing for Home component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Sidebar />
             <Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
         </BrowserRouter>
     );
 };
