5c456bf0815682190262784ba9011dcccec7e9e6
godwin <your_mail>
2026-03-22 18:46:47 +0100

    Refactor projectsData to use images array for multiple project images and added new projects

--- src/data/index.js
+++ src/data/index.js
@@ -21,11 +21,14 @@ export const projectsData = [
     {
         id: 1,
         title: "Asset Pulse HQ",
-		slug: "asset-pulse",
+        slug: "asset-pulse",
         category: "Asset Tracker Dashboard",
         description:
             "A comprehensive asset management dashboard built for businesses to track, manage and monitor their assets in real time. Features include asset categorization, depreciation tracking, and detailed reporting.",
-        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773918152/Macbook-Air-assetpulsehq-dashboard_zpreik.png",
+        images: [
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773918152/Macbook-Air-assetpulsehq-dashboard_zpreik.png",
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773918156/Macbook-Air-assetpulsehq-login_f7ju81.png",
+        ],
         tags: ["React", "Vite", "Supabase", "Tailwind"],
         liveUrl: "https://assetpulsehq.netlify.app",
         githubUrl: "https://github.com/Godwinn22/AssetPulse",
@@ -34,23 +37,66 @@ export const projectsData = [
     {
         id: 2,
         title: "Setime Digital Hub",
-		slug: "setime-digital-hub",
+        slug: "setime-digital-hub",
         category: "Coding Academy Platform",
         description:
             "An e-learning platform for a coding academy offering web development courses. Features include course management, student progress tracking, and an interactive coding environment.",
-        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774005656/Setime_Digital_Hub_ytrxc6.png",
+        images: [
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774005656/Setime_Digital_Hub_ytrxc6.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837441/Setime_Digital_Hub_About_l6fpjh.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837458/Setime_Digital_Hub_Courses_eb8smm.png"
+        ],
         tags: ["Wordpress", "Elementor", "MySQL"],
         liveUrl: "https://setimetechnologies.com",
         year: "2025",
     },
     {
         id: 3,
+        title: "JovitaChi Foundation",
+        slug: "jovitachi-foundation",
+        category: "NGO Foundation Website",
+        description:
+            "A website for a non-profit organization focused on educational, community empowerment etc. Features include Volunteers page, gallery, donation integration.",
+        images: [
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199648/Macbook-Air-jovitachifoundations_home_c9grdn.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199647/iPhone-12-PRO-jovitachifoundations_home_lpfhg9.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199674/Macbook-Air-jovitachifoundations_about_ggeion.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199624/Macbook-Air-jovitachifoundations_contact_yufikc.png"
+        ],
+        tags: ["Wordpress", "Elementor", "MySQL"],
+        liveUrl: "https://jovitachifoundations.com",
+        year: "2024",
+    },
+    {
+        id: 4,
+        title: "Supreme Package Machinery",
+        slug: "supreme-package-machinery",
+        category: "Industrial Machinery Website",
+        description:
+            "Supreme Package Machinery Ventures is a leading provider of packaging machinery solutions. The website features a product catalog, service offerings, and contact information to connect with potential clients in the industrial sector.",
+        images: [
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199629/Macbook-Air-supremepackagemachinery_home_oekhbi.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199651/iPhone-12-PRO-supremepackagemachinery_home_sgggcu.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199676/Macbook-Air-supremepackagemachinery_about_yr6krq.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774199665/Macbook-Air-supremepackagemachinery_services_dpn0fs.png",
+        ],
+        tags: ["HTML", "CSS", "JavaScript"],
+        liveUrl: "https://supremepackagemachinery.com",
+		githubUrl:"https://github.com/Godwinn22/supreme-package-machinery",
+        year: "2024",
+    },
+    {
+        id: 5,
         title: "Vermac Educational Foundation",
-		slug: "vermac-educational-foundation",
+        slug: "vermac-educational-foundation",
         category: "NGO Foundation Website",
         description:
             "A website for a non-profit organization focused on educational empowerment. Features include donation integration, event management, and a blog for sharing impact stories.",
-        image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837439/vermac_index_ccyhss.png",
+        images: [
+            "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837439/vermac_index_ccyhss.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837439/vermac_our_team_tipij4.png",
+			"https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837442/vermac_contact_us_o8hr94.png"
+        ],
         tags: ["Wordpress", "Elementor", "MySQL"],
         liveUrl: "https://vermaceducationalfoundation.com",
         year: "2024",
