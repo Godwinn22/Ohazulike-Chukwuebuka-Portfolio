7a71d7a7efd518dd354befaa26a7fa86459fbc71
godwin <your_mail>
2026-03-22 16:51:16 +0100

    Replaced recentProjects with projectsData.

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,6 @@
 export const myData = {
     name: "Ohazulike Chukwuebuka Godwin",
-	nick_name : "CodePanda",
+    nick_name: "CodePanda",
     tagline:
         "A Software Engineer who builds scalable products and loves to solve problems.",
     photo: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773911343/IMG-20230528-WA0041_4_nhhnoz.jpg",
@@ -17,23 +17,42 @@ export const statsData = [
     { number: "+5", label: "HAPPY\nCLIENTS" },
 ];
 
-export const recentProjects = [
+export const projectsData = [
     {
         id: 1,
         title: "Asset Pulse HQ",
+		slug: "asset-pulse",
         category: "Asset Tracker Dashboard",
+        description:
+            "A comprehensive asset management dashboard built for businesses to track, manage and monitor their assets in real time. Features include asset categorization, depreciation tracking, and detailed reporting.",
         image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773918152/Macbook-Air-assetpulsehq-dashboard_zpreik.png",
+        tags: ["React", "Vite", "Supabase", "Tailwind"],
+        liveUrl: "https://assetpulsehq.netlify.app",
+        githubUrl: "https://github.com/Godwinn22/AssetPulse",
+        year: "2026",
     },
     {
         id: 2,
-        title: "Setime Digital Hub Academy",
+        title: "Setime Digital Hub",
+		slug: "setime-digital-hub",
         category: "Coding Academy Platform",
+        description:
+            "An e-learning platform for a coding academy offering web development courses. Features include course management, student progress tracking, and an interactive coding environment.",
         image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1774005656/Setime_Digital_Hub_ytrxc6.png",
+        tags: ["Wordpress", "Elementor", "MySQL"],
+        liveUrl: "https://setimetechnologies.com",
+        year: "2025",
     },
     {
         id: 3,
         title: "Vermac Educational Foundation",
-        category: "NGO Foundation",
+		slug: "vermac-educational-foundation",
+        category: "NGO Foundation Website",
+        description:
+            "A website for a non-profit organization focused on educational empowerment. Features include donation integration, event management, and a blog for sharing impact stories.",
         image: "https://res.cloudinary.com/ddvs8i9lc/image/upload/v1773837439/vermac_index_ccyhss.png",
+        tags: ["Wordpress", "Elementor", "MySQL"],
+        liveUrl: "https://vermaceducationalfoundation.com",
+        year: "2024",
     },
 ];
