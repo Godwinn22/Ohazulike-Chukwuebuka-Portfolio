cbdfaae5a2bf6cfaa837b4b51061072fa801aa9a
godwin <your_mail>
2026-03-23 12:23:55 +0100

    Implement dark/light mode toggle functionality in App component for the Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -10,18 +11,35 @@ import Experience from "./pages/Experience";
 import TechStack from "./pages/TechStack";
 
 const App = () => {
+    // we default to dark mode
+    const [isDark, setIsDark] = useState(true);
+
+    const toggleTheme = () => {
+        setIsDark((prev) => {
+            if (prev) {
+                // If currently dark, switching to light
+                // Add "light" class to body
+                document.body.classList.add("light");
+            } else {
+                // If currently light, switching to dark
+                // Remove "light" class from body
+                document.body.classList.remove("light");
+            }
+            return !prev;
+        });
+    };
     return (
         <BrowserRouter>
             <Sidebar />
-            <Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/projects/:slug" element={<ProjectDetails />} />
-				<Route path="/experience" element={<Experience />} />
-				<Route path="/techstack" element={<TechStack />} />
-			</Routes>
-			<ContactForm />
+            <Navbar isDark={isDark} toggleTheme={toggleTheme} />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/projects/:slug" element={<ProjectDetails />} />
+                <Route path="/experience" element={<Experience />} />
+                <Route path="/techstack" element={<TechStack />} />
+            </Routes>
+            <ContactForm />
             <Footer />
         </BrowserRouter>
     );
