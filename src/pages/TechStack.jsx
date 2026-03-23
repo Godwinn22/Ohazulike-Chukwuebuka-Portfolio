4ef05faeea844b92a3b6d036803db0446e0ea6ee
godwin <your_mail>
2026-03-23 16:22:25 +0100

    Adjusted heading size in TechStack component for improved readability and responsiveness.

--- src/pages/TechStack.jsx
+++ src/pages/TechStack.jsx
@@ -12,7 +12,7 @@ const TechStack = () => {
             viewport={{ once: true }}
             className="ml-0 md:ml-90 mt-2 md:mt-8 min-h-screen px-8 md:px-16 pt-31 pb-24"
         >
-            <h1 className="font-display text-[50px] md:text-[80px] font-black uppercase mb-8 leading-none">
+            <h1 className="font-display text-[35px] md:text-[70px] font-black uppercase mb-8 leading-none">
                 <span className="text-text-primary block">Tech</span>
                 <span
                     className="block"
